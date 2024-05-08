afae0de67a6c4aada976488e82e831a50b15e13e
Bartek Rutkowski <barrutko.eth@gmail.com>
2024-05-08 19:00:31 +0200

    feat: add logic

--- web3-functions/kill-switch/index.ts
+++ web3-functions/kill-switch/index.ts
@@ -1,7 +1,7 @@
 import { Contract } from '@ethersproject/contracts'
 import { Web3Function, Web3FunctionContext } from '@gelatonetwork/web3-functions-sdk'
 
-import { killSwitchOracleAbi, multicallAbi } from '../../abis'
+import { killSwitchOracleAbi, multicallAbi, oracleAbi } from '../../abis'
 import { addresses } from '../../utils'
 
 Web3Function.onRun(async (context: Web3FunctionContext) => {
@@ -10,12 +10,58 @@ Web3Function.onRun(async (context: Web3FunctionContext) => {
     const provider = multiChainProvider.default()
 
     const multicall = new Contract(addresses.mainnet.multicall, multicallAbi, provider)
-    const killSwitchOracle = new Contract(addresses.mainnet.killSwitchOracle,killSwitchOracleAbi, provider)
+    const killSwitchOracle = new Contract(addresses.mainnet.killSwitchOracle, killSwitchOracleAbi, provider)
 
-    const oracles = await killSwitchOracle.oracles()
+    const oracleAddresses = await killSwitchOracle.oracles()
 
-    console.log({oracles})
-    console.log(multicall.address)
+    let multicallCalls: Array<{ target: string; callData: string }> = []
+
+    for (const oracleAddress of oracleAddresses) {
+        const oracle = new Contract(oracleAddress, oracleAbi, provider)
+        multicallCalls = [
+            ...multicallCalls,
+            ...[
+                {
+                    target: oracleAddress,
+                    callData: oracle.interface.encodeFunctionData('latestAnswer'),
+                },
+                {
+                    target: addresses.mainnet.killSwitchOracle,
+                    callData: killSwitchOracle.interface.encodeFunctionData('oracleThresholds', [oracleAddress]),
+                },
+                {
+                    target: addresses.mainnet.killSwitchOracle,
+                    callData: killSwitchOracle.interface.encodeFunctionData('triggered'),
+                },
+            ],
+        ]
+    }
+
+    const multicallResults = (await multicall.callStatic.aggregate(multicallCalls)).returnData
+
+    for (const oracleAddress of oracleAddresses) {
+        const oracle = new Contract(oracleAddress, oracleAbi, provider)
+
+        const latestAnswer = BigInt(
+            oracle.interface.decodeFunctionResult('latestAnswer', multicallResults[0])[0].toString(),
+        )
+        const threshold = BigInt(
+            killSwitchOracle.interface.decodeFunctionResult('oracleThresholds', multicallResults[1])[0].toString(),
+        )
+        const triggered = killSwitchOracle.interface.decodeFunctionResult('triggered', multicallResults[2])[0]
+
+        if (!triggered && latestAnswer > 0 && latestAnswer <= threshold) {
+            return {
+                canExec: true,
+                callData: [
+                    {
+                        to: addresses.mainnet.killSwitchOracle,
+                        data: killSwitchOracle.interface.encodeFunctionData('trigger', [oracleAddress]),
+                    },
+                ],
+            }
+        }
+    }
 
     return {
         canExec: false,
