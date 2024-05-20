7c8679d2c3735f595154d46108467f7153a3689f
Bartek Rutkowski <barrutko.eth@gmail.com>
2024-05-20 09:50:00 +0200

    refactor: add maximum code depth

--- utils/insistOnExecution.ts
+++ utils/insistOnExecution.ts
@@ -1,15 +1,17 @@
-export const insistOnExecution = async (callToExecute: () => Promise<any>): Promise<any> => {
+export const insistOnExecution = async (callToExecute: () => Promise<any>, callDepth = 0): Promise<any> => {
     try {
         return await callToExecute()
     } catch (error) {
         const { message } = error as any
-        if (
-            message.includes('Transaction reverted without a reason') ||
-            message.includes('Transaction ran out of gas')
-        ) {
-            return await insistOnExecution(callToExecute)
-        } else {
-            throw error
+        console.log('retrying', callDepth)
+
+        const messageHasAReasonToIgnore =
+            message.includes('Transaction reverted without a reason') || message.includes('Transaction ran out of gas')
+
+        if (messageHasAReasonToIgnore && callDepth < 10) {
+            return await insistOnExecution(callToExecute, callDepth + 1)
         }
+
+        throw error
     }
 }
