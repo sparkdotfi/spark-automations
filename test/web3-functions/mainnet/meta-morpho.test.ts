66ba99e1f3a7488b3061adc74afea20199948e42
Bartek Rutkowski <barrutko.eth@gmail.com>
2024-06-05 15:08:09 +0200

    feat: use events to track pending cap updates (#9)

--- test/web3-functions/mainnet/meta-morpho.test.ts
+++ test/web3-functions/mainnet/meta-morpho.test.ts
@@ -25,27 +25,20 @@ describe('MetaMorpho', function () {
     let metaMorphoVault: Contract
 
     // Markets to monitor for cap changes
-    const userArgs = {
-        marketParams_loanToken: [
-            '0x6B175474E89094C44Da98b954EedeAC495271d0F', // DAI
-            '0x6B175474E89094C44Da98b954EedeAC495271d0F', // DAI
-        ],
-        marketParams_collateralToken: [
-            '0x9D39A5DE30e57443BfF2A8307A4256c8797A3497', // SUSDE
-            '0x4c9EDD5852cd905f086C759E8383e09bff1E68B3', // USDE
-        ],
-        marketParams_oracle: [
-            '0x5D916980D5Ae1737a8330Bf24dF812b2911Aae25', // SUSDE ORACLE
-            '0xaE4750d0813B5E37A51f7629beedd72AF1f9cA35', // USDE ORACLE
-        ],
-        marketParams_irm: [
-            '0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC', // DEFAULT IRM
-            '0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC', // DEFAULT IRM
-        ],
-        marketParams_lltv: [
-            '860000000000000000', // 0.86
-            '860000000000000000', // 0.86
-        ],
+    const susdeMarket = {
+        loanToken: '0x6B175474E89094C44Da98b954EedeAC495271d0F', // DAI
+        collateralToken: '0x9D39A5DE30e57443BfF2A8307A4256c8797A3497', // SUSDE
+        oracle: '0x5D916980D5Ae1737a8330Bf24dF812b2911Aae25', // SUSDE ORACLE
+        irm: '0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC', // DEFAULT IRM
+        lltv: '860000000000000000', // 0.86
+    }
+
+    const usdeMarket = {
+        loanToken: '0x6B175474E89094C44Da98b954EedeAC495271d0F', // DAI
+        collateralToken: '0x4c9EDD5852cd905f086C759E8383e09bff1E68B3', // USDE
+        oracle: '0xaE4750d0813B5E37A51f7629beedd72AF1f9cA35', // USDE ORACLE
+        irm: '0x870aC11D48B15DB9a138Cf899d20F13F79Ba00BC', // DEFAULT IRM
+        lltv: '860000000000000000', // 0.86
     }
 
     let susdeMarketId: string
@@ -60,18 +53,18 @@ describe('MetaMorpho', function () {
         executor = await ethers.getSigner(addresses.mainnet.executor)
 
         susdeMarketId = calculateMetaMorphoMarketId(
-            userArgs.marketParams_loanToken[0],
-            userArgs.marketParams_collateralToken[0],
-            userArgs.marketParams_oracle[0],
-            userArgs.marketParams_irm[0],
-            userArgs.marketParams_lltv[0],
+            susdeMarket.loanToken,
+            susdeMarket.collateralToken,
+            susdeMarket.oracle,
+            susdeMarket.irm,
+            susdeMarket.lltv,
         )
         usdeMarketId = calculateMetaMorphoMarketId(
-            userArgs.marketParams_loanToken[1],
-            userArgs.marketParams_collateralToken[1],
-            userArgs.marketParams_oracle[1],
-            userArgs.marketParams_irm[1],
-            userArgs.marketParams_lltv[1],
+            usdeMarket.loanToken,
+            usdeMarket.collateralToken,
+            usdeMarket.oracle,
+            usdeMarket.irm,
+            usdeMarket.lltv,
         )
 
         metaMorphoVault = new Contract(addresses.mainnet.metaMorpho, metaMorphoAbi, executor)
@@ -94,7 +87,7 @@ describe('MetaMorpho', function () {
     })
 
     it('no pending caps', async () => {
-        const { result } = await metaMorphoW3F.run('onRun', { userArgs })
+        const { result } = await metaMorphoW3F.run('onRun')
 
         expect(result.canExec).to.equal(false)
         !result.canExec && expect(result.message).to.equal('No pending caps to be accepted')
@@ -103,20 +96,11 @@ describe('MetaMorpho', function () {
     it('no pending caps (waiting for the timelock)', async () => {
         const susdeCurrentCap = (await metaMorphoVault.config(susdeMarketId)).cap
 
-        await metaMorphoVault.submitCap(
-            [
-                userArgs.marketParams_loanToken[0],
-                userArgs.marketParams_collateralToken[0],
-                userArgs.marketParams_oracle[0],
-                userArgs.marketParams_irm[0],
-                userArgs.marketParams_lltv[0],
-            ],
-            susdeCurrentCap.add(1),
-        )
+        await metaMorphoVault.submitCap(Object.values(susdeMarket), susdeCurrentCap.add(1))
 
         await mine(2, { interval: timelock.sub(1) })
 
-        const { result } = await metaMorphoW3F.run('onRun', { userArgs })
+        const { result } = await metaMorphoW3F.run('onRun')
 
         expect(result.canExec).to.equal(false)
         !result.canExec && expect(result.message).to.equal('No pending caps to be accepted')
@@ -125,26 +109,17 @@ describe('MetaMorpho', function () {
     it('one cap update to be accepted', async () => {
         const susdeInitialCap = (await metaMorphoVault.config(susdeMarketId)).cap
 
-        await metaMorphoVault.submitCap(
-            [
-                userArgs.marketParams_loanToken[0],
-                userArgs.marketParams_collateralToken[0],
-                userArgs.marketParams_oracle[0],
-                userArgs.marketParams_irm[0],
-                userArgs.marketParams_lltv[0],
-            ],
-            susdeInitialCap.add(1),
-        )
+        await metaMorphoVault.submitCap(Object.values(susdeMarket), susdeInitialCap.add(1))
         await mine(2, { interval: timelock.sub(1) })
 
-        const { result: negativeResult } = await metaMorphoW3F.run('onRun', { userArgs })
+        const { result: negativeResult } = await metaMorphoW3F.run('onRun')
 
         expect(negativeResult.canExec).to.equal(false)
         !negativeResult.canExec && expect(negativeResult.message).to.equal('No pending caps to be accepted')
 
         await mine(2, { interval: 1 })
 
-        const { result: positiveResult } = await metaMorphoW3F.run('onRun', { userArgs })
+        const { result: positiveResult } = await metaMorphoW3F.run('onRun')
 
         expect(positiveResult.canExec).to.equal(true)
         if (!positiveResult.canExec) {
@@ -155,15 +130,7 @@ describe('MetaMorpho', function () {
         expect(callData).to.deep.equal([
             {
                 to: addresses.mainnet.metaMorpho,
-                data: metaMorphoVault.interface.encodeFunctionData('acceptCap', [
-                    [
-                        userArgs.marketParams_loanToken[0],
-                        userArgs.marketParams_collateralToken[0],
-                        userArgs.marketParams_oracle[0],
-                        userArgs.marketParams_irm[0],
-                        userArgs.marketParams_lltv[0],
-                    ],
-                ]),
+                data: metaMorphoVault.interface.encodeFunctionData('acceptCap', [Object.values(susdeMarket)]),
             },
         ])
 
@@ -191,30 +158,12 @@ describe('MetaMorpho', function () {
         const susdeInitialCap = (await metaMorphoVault.config(susdeMarketId)).cap
         const usdeInitialCap = (await metaMorphoVault.config(usdeMarketId)).cap
 
-        await metaMorphoVault.submitCap(
-            [
-                userArgs.marketParams_loanToken[0],
-                userArgs.marketParams_collateralToken[0],
-                userArgs.marketParams_oracle[0],
-                userArgs.marketParams_irm[0],
-                userArgs.marketParams_lltv[0],
-            ],
-            susdeInitialCap.add(1),
-        )
-        await metaMorphoVault.submitCap(
-            [
-                userArgs.marketParams_loanToken[1],
-                userArgs.marketParams_collateralToken[1],
-                userArgs.marketParams_oracle[1],
-                userArgs.marketParams_irm[1],
-                userArgs.marketParams_lltv[1],
-            ],
-            usdeInitialCap.add(1),
-        )
+        await metaMorphoVault.submitCap(Object.values(susdeMarket), susdeInitialCap.add(1))
+        await metaMorphoVault.submitCap(Object.values(usdeMarket), usdeInitialCap.add(1))
 
         await mine(2, { interval: timelock })
 
-        const { result } = await metaMorphoW3F.run('onRun', { userArgs })
+        const { result } = await metaMorphoW3F.run('onRun')
 
         expect(result.canExec).to.equal(true)
         if (!result.canExec) {
@@ -225,27 +174,11 @@ describe('MetaMorpho', function () {
         expect(callData).to.deep.equal([
             {
                 to: addresses.mainnet.metaMorpho,
-                data: metaMorphoVault.interface.encodeFunctionData('acceptCap', [
-                    [
-                        userArgs.marketParams_loanToken[0],
-                        userArgs.marketParams_collateralToken[0],
-                        userArgs.marketParams_oracle[0],
-                        userArgs.marketParams_irm[0],
-                        userArgs.marketParams_lltv[0],
-                    ],
-                ]),
+                data: metaMorphoVault.interface.encodeFunctionData('acceptCap', [Object.values(susdeMarket)]),
             },
             {
                 to: addresses.mainnet.metaMorpho,
-                data: metaMorphoVault.interface.encodeFunctionData('acceptCap', [
-                    [
-                        userArgs.marketParams_loanToken[1],
-                        userArgs.marketParams_collateralToken[1],
-                        userArgs.marketParams_oracle[1],
-                        userArgs.marketParams_irm[1],
-                        userArgs.marketParams_lltv[1],
-                    ],
-                ]),
+                data: metaMorphoVault.interface.encodeFunctionData('acceptCap', [Object.values(usdeMarket)]),
             },
         ])
 
