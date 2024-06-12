154bb0b5ac00d94c56c0614afd1080c54347196c
Bartek Rutkowski <barrutko.eth@gmail.com>
2024-06-12 15:19:07 +0200

    fix: formatting

--- scripts/checkDeployerKey.ts
+++ scripts/checkDeployerKey.ts
@@ -1,16 +1,16 @@
 import * as fs from 'fs'
-import { ethers } from "ethers"
+import { ethers } from 'ethers'
 
 console.log('== Testing access to the deployer key ==')
 
-const keystorePath = process.argv[2] || process.env.KEYSTORE_PATH as string
-const passwordPath = process.argv[3] || process.env.PASSWORD_PATH as string
+const keystorePath = process.argv[2] || (process.env.KEYSTORE_PATH as string)
+const passwordPath = process.argv[3] || (process.env.PASSWORD_PATH as string)
 
 console.log('   * Keystore path:', keystorePath)
 console.log('   * Password path:', passwordPath || 'No password path provided')
 
-const password = passwordPath ? fs.readFileSync(passwordPath,'utf8').slice(0, -1) : ''
-const keystore = fs.readFileSync(keystorePath,'utf8')
+const password = passwordPath ? fs.readFileSync(passwordPath, 'utf8').slice(0, -1) : ''
+const keystore = fs.readFileSync(keystorePath, 'utf8')
 const deployer = ethers.Wallet.fromEncryptedJsonSync(keystore, password)
 
 console.log('   * Deployer:     ', deployer.address)
--- scripts/uploadToIpfs.ts
+++ scripts/uploadToIpfs.ts
@@ -3,8 +3,8 @@ import fs from 'fs'
 const w3fName = process.argv[2]
 
 if (!w3fName) {
-  console.error('Please provide a w3f name')
-  process.exit(1)
+    console.error('Please provide a w3f name')
+    process.exit(1)
 }
 console.log(`Uploading ${w3fName} to IPFS...`)
 
