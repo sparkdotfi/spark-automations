0d70df4ffb66624a18df75360686ae6238159d5b
Bartek Rutkowski <barrutko.eth@gmail.com>
2024-06-12 14:27:00 +0200

    fix: add minor adjustments

--- scripts/uploadToIpfs.ts
+++ scripts/uploadToIpfs.ts
@@ -10,11 +10,11 @@ console.log(`Uploading ${w3fName} to IPFS...`)
 
 const deploymentOutput = require('child_process').execSync(`npx hardhat w3f-deploy ${w3fName}`)
 
-const cid = deploymentOutput.toString().split(' ').slice(-1)[0].slice(0, -1)
+const cid = deploymentOutput.toString().split(' ')[8].slice(0, -4)
 console.log(`New ${w3fName} CID: ${cid}`)
 
 const ipfsDeployments = JSON.parse(fs.readFileSync('./scripts/pre-deployments.json'))
 ipfsDeployments[w3fName] = cid
 
-fs.writeFileSync('./scripts/pre-deployments.json', JSON.stringify(ipfsDeployments, null, 2).concat('\n'))
+fs.writeFileSync('./scripts/pre-deployments.json', JSON.stringify(ipfsDeployments, null, 4).concat('\n'))
 console.log('"pre-deployments.json" file successfully updated!')
