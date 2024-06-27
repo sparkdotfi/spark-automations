097ec09972d5a8e50c74bce09b750e10d828b37a
Bartek Rutkowski <barrutko.eth@gmail.com>
2024-06-27 13:22:40 +0200

    feat: finish refactoring

--- web3-functions/xchain-oracle-ticker/index.ts
+++ web3-functions/xchain-oracle-ticker/index.ts
@@ -1,12 +1,12 @@
 import { Contract } from '@ethersproject/contracts'
-import { BigNumber, utils, providers } from 'ethers'
 import { Web3Function, Web3FunctionContext } from '@gelatonetwork/web3-functions-sdk'
 import axios from 'axios'
+import { providers, utils } from 'ethers'
 
 import { forwarderAbi, forwarderArbitrumAbi, multicallAbi, potAbi } from '../../abis'
 import { addresses, sendMessageToSlack } from '../../utils'
 
-const arbitrumDomainUrls: Record<string, string> = {
+const arbitrumDomainUrls: Record<string, string | undefined> = {
     [`${addresses.mainnet.dsrForwarders.arbitrumStyle.arbitrum}`]: 'https://arb1.arbitrum.io/rpc',
 }
 
@@ -20,7 +20,8 @@ Web3Function.onRun(async (context: Web3FunctionContext) => {
     const sendSlackMessages = userArgs.sendSlackMessages as boolean
 
     const slackWebhookUrl = (await secrets.get('SLACK_WEBHOOK_URL')) as string
-    const generateSlackMessage = (domainAlias: string) => `\`\`\`🦾🔮 DSR Oracle Keeper 🦾🔮\nFeed refresh to be sent to ${domainAlias}\`\`\``
+    const generateSlackMessage = (domainAlias: string): string =>
+        `\`\`\`🦾🔮 DSR Oracle Keeper 🦾🔮\nFeed refresh to be sent to ${domainAlias}\`\`\``
 
     const optimismStyleForwarderAddresses = addresses.mainnet.dsrForwarders.optimismStyle
     const arbitrumStyleForwarderAddresses = addresses.mainnet.dsrForwarders.arbitrumStyle
@@ -30,13 +31,16 @@ Web3Function.onRun(async (context: Web3FunctionContext) => {
     const currentDsr = await pot.dsr()
     const latestTimestamp = await provider.getBlock('latest').then((block) => block.timestamp)
 
-    const callsToExecute: Array<{to: string; data: string}> = []
+    const callsToExecute: Array<{ to: string; data: string }> = []
     const slackMessages: Array<string> = []
 
     const multicallCalls: Array<{ target: string; callData: string }> = []
 
     const forwarderInterface = new utils.Interface(forwarderAbi)
-    for (const forwarderAddress of [...Object.values(optimismStyleForwarderAddresses), ...Object.values(arbitrumStyleForwarderAddresses)]) {
+    for (const forwarderAddress of [
+        ...Object.values(optimismStyleForwarderAddresses),
+        ...Object.values(arbitrumStyleForwarderAddresses),
+    ]) {
         multicallCalls.push({
             target: forwarderAddress,
             callData: forwarderInterface.encodeFunctionData('getLastSeenPotData', []),
@@ -45,11 +49,14 @@ Web3Function.onRun(async (context: Web3FunctionContext) => {
     let multicallResults = (await multicall.callStatic.aggregate(multicallCalls)).returnData
 
     for (const [domain, forwarderAddress] of Object.entries(optimismStyleForwarderAddresses)) {
-        const lastForwardedPotData = forwarderInterface.decodeFunctionResult('getLastSeenPotData', multicallResults[0])[0]
+        const lastForwardedPotData = forwarderInterface.decodeFunctionResult(
+            'getLastSeenPotData',
+            multicallResults[0],
+        )[0]
         multicallResults = multicallResults.slice(1)
         if (
-            (BigInt(lastForwardedPotData.dsr) != BigInt(currentDsr)) ||
-            (BigInt(latestTimestamp) > (BigInt(lastForwardedPotData.rho) + maxDelta))
+            BigInt(lastForwardedPotData.dsr) != BigInt(currentDsr) ||
+            BigInt(latestTimestamp) > BigInt(lastForwardedPotData.rho) + maxDelta
         ) {
             slackMessages.push(generateSlackMessage(domain))
             callsToExecute.push({
@@ -70,11 +77,14 @@ Web3Function.onRun(async (context: Web3FunctionContext) => {
         const baseFee = (await provider.getGasPrice()).mul(120).div(100)
         const maxFeePerGas = (await arbitrumProvider.getGasPrice()).mul(120).div(100)
 
-        const lastForwardedPotData = forwarderInterface.decodeFunctionResult('getLastSeenPotData', multicallResults[0])[0]
+        const lastForwardedPotData = forwarderInterface.decodeFunctionResult(
+            'getLastSeenPotData',
+            multicallResults[0],
+        )[0]
         multicallResults = multicallResults.slice(1)
         if (
-            (BigInt(lastForwardedPotData.dsr) != BigInt(currentDsr)) ||
-            (BigInt(latestTimestamp) > (BigInt(lastForwardedPotData.rho) + maxDelta))
+            BigInt(lastForwardedPotData.dsr) != BigInt(currentDsr) ||
+            BigInt(latestTimestamp) > BigInt(lastForwardedPotData.rho) + maxDelta
         ) {
             slackMessages.push(generateSlackMessage(domain))
             callsToExecute.push({
