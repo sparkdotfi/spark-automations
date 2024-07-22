32b60da612093c70595a602efdfe0a9c397ee65b
Bartek Rutkowski <barrutko.eth@gmail.com>
2024-07-22 11:57:44 +0200

    feat: add thousand separation helper

--- utils/formatArt.ts
+++ utils/formatArt.ts
@@ -1,7 +1,9 @@
+import { formatThousandSeparators } from './formatThousandSeparators'
+
 export const formatArt = (art: string): string => {
     const weiTail = art.slice(-18)
     const wholeUnits = art.slice(0, -18)
-    const formattedWholeUnits = wholeUnits.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+    const formattedWholeUnits = formatThousandSeparators(wholeUnits)
 
     return [formattedWholeUnits, weiTail].join('.')
 }
