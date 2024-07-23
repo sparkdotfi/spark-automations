d1270339fbca49d7a9d320566c408935edb9714f
Bartek Rutkowski <barrutko.eth@gmail.com>
2024-07-23 19:27:56 +0200

    feat: create script to upload everything to ipfs in one shot (#20)
    * feat: squash dsr oracle keeper messages into one
    
    * feat: improve d3m keeper messaging
    
    * feat: add reason to dsr oracle keepr message
    
    * fix: linting
    
    * feat: add thousand separation helper
    
    * feat: improve cap automator messages
    
    * refactor: rename formatArt to format18DigitPrecision
    
    * feat: improve morpho keeper messages
    
    * fix: formatting
    
    * test: test formatThousandSeparators
    
    * feat: update ipfs script to upload all of the keepers in one run

--- scripts/uploadToIpfs.ts
+++ scripts/uploadToIpfs.ts
@@ -1,20 +1,22 @@
 import fs from 'fs'
+import path from 'path'
 
-const w3fName = process.argv[2]
-
-if (!w3fName) {
-    console.error('Provide a w3f name')
-    process.exit(1)
+function listDirectories(srcPath: string): string[] {
+    return fs.readdirSync(srcPath).filter((file) => fs.statSync(path.join(srcPath, file)).isDirectory())
 }
-console.log(`Uploading ${w3fName} to IPFS...`)
 
-const deploymentOutput = require('child_process').execSync(`npx hardhat w3f-deploy ${w3fName}`)
-
-const cid = deploymentOutput.toString().split(' ')[8].slice(0, -4)
-console.log(`New ${w3fName} CID: ${cid}`)
+const w3fNames = listDirectories('./web3-functions')
 
 const ipfsDeployments = JSON.parse(fs.readFileSync('./scripts/pre-deployments.json'))
-ipfsDeployments[w3fName] = cid
+
+for (const w3fName of w3fNames) {
+    console.log(`Uploading ${w3fName} to IPFS...`)
+
+    const deploymentOutput = require('child_process').execSync(`npx hardhat w3f-deploy ${w3fName}`)
+    const cid = deploymentOutput.toString().split(' ')[8].slice(0, -4)
+    console.log(`New ${w3fName} CID: ${cid}`)
+
+    ipfsDeployments[w3fName] = cid
+}
 
 fs.writeFileSync('./scripts/pre-deployments.json', JSON.stringify(ipfsDeployments, null, 4).concat('\n'))
-console.log('"pre-deployments.json" file successfully updated!')
